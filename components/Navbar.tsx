2bbc81f93099f760722d9c9ca3a73a698a6c6c7c
saied-nasiripour <saied.nasiripour@gmail.com>
2024-05-12 16:50:22 +0330

    chane favicon

---
+++ components/Navbar.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import Image from "next/image";
+
+
+const Navbar = () => {
+    return (
+        <nav className="flexBetween max-container padding-container relative z-30 py-5">
+            <Link href={"/"}>
+                <Image src="/hilink-logo.svg" alt="logo" width={74} height={26}/>
+            </Link>
+        </nav>
+    );
+};
+
+export default Navbar;
