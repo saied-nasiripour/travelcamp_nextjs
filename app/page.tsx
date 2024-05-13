b4e79c435f29af01853778a22a0c8c3baafa519d
saied-nasiripour <saied.nasiripour@gmail.com>
2024-05-13 15:40:09 +0330

    hero section is done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import Hero from "@/components/Hero";
+import Camp from "@/components/Camp";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
 
 
 export default function Home() {
     return (
-        <h1 className="font-bold text-teal-500 text-2xl">Hello World</h1>
+        <>
+            <Hero/>
+            <Camp/>
+            <Guide/>
+            <Features/>
+            <GetApp/>
+        </>
     );
 }
